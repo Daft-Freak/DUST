0749a87d54d25ecdb8f0212e8af07e787efe0166
Charlie Birks <charlie@daft.games>
2021-11-02 14:49:36 +0000

    Add a font and a little menu

--- main.cpp
+++ main.cpp
@@ -2,7 +2,49 @@
 #include <gba/gba.hpp>
 #include <gba/ext/agbabi.hpp>
 
+#include "assets/8x8font.h"
+
 using namespace gba;
+
+static auto palRAM = reinterpret_cast<uint16_t *>(0x05000000);
+static auto videoRAM = reinterpret_cast<uint16_t *>(0x06000000);
+
+io::keypad_manager keypad_man;
+
+static void load_font() {
+    palRAM[0] = 0;
+    palRAM[1] = 0x7FFF;
+
+    bios::bit_un_pack_input unpack {
+        .size = sizeof(font8x8_char_data_1bpp),
+        .source_width = bios::un_pack_bits::_1,
+        .destination_width = bios::un_pack_bits::_4
+    };
+    bios::bit_un_pack(font8x8_char_data_1bpp, videoRAM, &unpack);
+}
+
+static void write_text(int x, int y, const char *text) {
+    // char block 2
+    auto ptr = videoRAM + 0x800 + x + y * 32;
+
+    for(; *text; text++)
+        *ptr++ = *text - ' ';
+}
+
+static void placeholder(){
+    palRAM[1] = 0x7C00;
+}
+
+struct test_info {
+    const char *name;
+    void (*func)();
+};
+
+static test_info tests[] {
+    {"Test A" , placeholder},
+    {"Test B" , placeholder},
+    {"Test C" , placeholder}
+};
 
 int main(int argc, char * argv[]) {
     reg::dispstat::write({.vblank_irq = true});
@@ -14,12 +56,40 @@ int main(int argc, char * argv[]) {
         .layer_background_0 = true,
     });
 
-    io::keypad_manager keypad;
+    reg::bg0cnt::write(background_control {
+        .character_base_block = 0,
+        .color_depth = color_depth::bpp_4,
+        .screen_base_block = 2,
+        .screen_size = screen_size::regular_32x32
+    });
+
+    // menu
+    load_font();
+    write_text(0, 1, "Dafts Useful(?) Suite of Tests");
+
+    int y = 3;
+    for(auto &test : tests)
+        write_text(2, y++, test.name);
+
+    int item = 0;
 
     while(true) {
-        keypad.poll();
+        keypad_man.poll();
 
         bios::vblank_intr_wait();
+
+        if(keypad_man.any_switched_down(key_mask::make(key::up, key::down))) {
+            write_text(1, item + 3, " ");
+
+            int32_t num_tests = std::size(tests);
+            item = std::max(0l, std::min(num_tests - 1, item + keypad_man.axis_y()));
+        }
+
+        if(keypad_man.switched_up(key::button_a)) {
+            tests[item].func();
+        }
+
+        write_text(1, item + 3, ">");
     }
 
     __builtin_unreachable();
